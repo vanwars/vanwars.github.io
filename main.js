3b0195116aa57647951c7959ecb9b0c693ab985e
Sarah <vanwars@gmail.com>
2020-03-20 10:41:51 -0500

    updates to portfolio

--- main.js
+++ main.js
@@ -8,6 +8,20 @@ const attachEventHandlers = () => {
     document.querySelector('.menu-toggle').onclick = (e) => {
         document.querySelector('nav').classList.toggle('mobile');
     };
+
+    for (elem of document.querySelectorAll('nav a')) {
+        elem.onclick = scrollToAnchor;
+    }
 };
+
+const scrollToAnchor = (ev) => {
+    const distanceToTop = (el) => {
+        return Math.floor(el.getBoundingClientRect().top);
+    };
+	var targetID = ev.target.getAttribute('href');
+	const targetAnchor = document.querySelector(targetID);
+    window.scrollBy({ top: distanceToTop(targetAnchor), left: 0, behavior: 'smooth' });
+    ev.preventDefault();
+}
 
 attachEventHandlers();
